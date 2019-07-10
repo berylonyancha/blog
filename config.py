060f64284930beff327406c0c2fc1f5b047c896d
Beryl Onyancha <berylonyancha@gmail>
2019-07-10 11:11:46 +0300

    made changes to the email and password authentication

--- config.py
+++ config.py
@@ -3,16 +3,22 @@ class Config:
     """
     General configuration parent class
     """
-    SECRET_KEY= os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://beryl:123@localhost/blogs'
-    UPLOADED_PHOTOS_DEST = 'app/static/photos'
-    
-    #email configurations
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+   SECRET_KEY = "beryl55"
+   UPLOADED_PHOTOS_DEST = 'app/static/photos'
+   SSL_DISABLE = False
+   SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+   SQLCLCHEMY_RECORD_QUERIES = True
+   CSRF_ENABLED = True
+   MAIL_SERVER = os.environ.get('MAIL_SERVER','smtp.gmail.com')
+   MAIL_USERNAME = os.environ.get('MAIL_USERNAME','berylonyancha@gmail.com')
+   MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD','november051999')
+   MAIL_SENDER = os.environ.get('MAIL_SENDER', 'Project Admin<berylonyancha@gmail.com>')
+   PROJECT_ADMIN = os.environ.get('PROJECT_ADMIN', 'PROJECT_ADMIN')
+   MAIL_PORT = int(os.environ.get('MAIL_PORT', '465'))
+   MAIL_USE_TLS = int(os.environ.get('MAIL_USE_TLS', False))
+   MAIL_USE_SSL = int(os.environ.get('MAIL_USE_SSL',  True))
+   MAIL_SUBJECT_PREFIX = '[PROJECT]'
+
 
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
