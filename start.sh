a4a0c69d02f1d3e01efc3c62d045485ad982ff82
Beryl Onyancha <berylonyancha@gmail>
2019-07-10 11:19:06 +0300

    made changes to the email and password authentication

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export SECRET_KEY="beryl55"
 
-export MAIL_USERNAME="berylonyancha@gmail.com"
-export MAIL_PASSWORD="november051999"
+export MAIL_USERNAME="florenceshiru765@gmail.com"
+export MAIL_PASSWORD="@254generari237"
 
 python3 manage.py server
